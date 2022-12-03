826b957b0235513eb2afbf35be84e6a6b75270d9
Kushagra Sharma <kushagra.sharma@elevate.so>
2022-12-03 07:37:04 +0530

    second-commit

---
+++ src/JS/dropdown.js
@@ -0,0 +1,9 @@
+const changeColor = (getColor) => {
+    let bg = document.querySelector('.bg');
+    let selectColor = getColor.value;
+    let text = getColor.options[getColor.selectedIndex].text;
+    bg.style.background = selectColor;
+    bg.innerHTML = text;
+}
+
+module.exports = changeColor
